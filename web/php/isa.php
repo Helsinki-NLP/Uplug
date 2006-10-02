f3e5b67195c5a3e9e13eb13f0f40894bf90b0698
joerg72 <joerg72>
2006-10-02 21:35:45 +0000

    *** empty log message ***

--- web/php/isa.php
+++ web/php/isa.php
@@ -52,7 +52,7 @@ if (isset($_POST['corpus'])){
 }
 
 
-header('Content-Type: text/html; charset=ISO-8859-1');
+header('Content-Type: text/html; charset=UTF8');
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
                       "http://www.w3.org/TR/html4/loose.dtd">
