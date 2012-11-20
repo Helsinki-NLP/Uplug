b4b8490a3d6cea5f872d2b54648f3100b6c6baa5
tiedemann <tiedeman@gmail.com>
2012-11-20 16:21:40 +0100

    all ext moved to share

---
+++ share/ext/chasen/config.h
@@ -0,0 +1,91 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define if you have a working `mmap' system call.  */
+/* #undef HAVE_MMAP */
+
+/* Define if you have <sys/wait.h> that is POSIX.1 compatible.  */
+#define HAVE_SYS_WAIT_H 1
+
+/* Define to `long' if <sys/types.h> doesn't define.  */
+/* #undef off_t */
+
+/* Define as the return type of signal handlers (int or void).  */
+#define RETSIGTYPE void
+
+/* Define to `unsigned' if <sys/types.h> doesn't define.  */
+/* #undef size_t */
+
+/* Define if you have the ANSI C header files.  */
+/* #undef STDC_HEADERS */
+
+/* Define if your processor stores words with the most significant
+   byte first (like Motorola and SPARC, unlike Intel and VAX).  */
+/* #undef WORDS_BIGENDIAN */
+
+/* Define if you have the getpagesize function.  */
+#define HAVE_GETPAGESIZE 1
+
+/* Define if you have the htonl function.  */
+#define HAVE_HTONL 1
+
+/* Define if you have the <dirent.h> header file.  */
+#define HAVE_DIRENT_H 1
+
+/* Define if you have the <dlfcn.h> header file.  */
+#define HAVE_DLFCN_H 1
+
+/* Define if you have the <fcntl.h> header file.  */
+#define HAVE_FCNTL_H 1
+
+/* Define if you have the <io.h> header file.  */
+/* #undef HAVE_IO_H */
+
+/* Define if you have the <limits.h> header file.  */
+#define HAVE_LIMITS_H 1
+
+/* Define if you have the <ndir.h> header file.  */
+/* #undef HAVE_NDIR_H */
+
+/* Define if you have the <netinet/in.h> header file.  */
+#define HAVE_NETINET_IN_H 1
+
+/* Define if you have the <stdlib.h> header file.  */
+#define HAVE_STDLIB_H 1
+
+/* Define if you have the <sys/dir.h> header file.  */
+/* #undef HAVE_SYS_DIR_H */
+
+/* Define if you have the <sys/file.h> header file.  */
+#define HAVE_SYS_FILE_H 1
+
+/* Define if you have the <sys/ndir.h> header file.  */
+/* #undef HAVE_SYS_NDIR_H */
+
+/* Define if you have the <sys/param.h> header file.  */
+#define HAVE_SYS_PARAM_H 1
+
+/* Define if you have the <sys/stat.h> header file.  */
+#define HAVE_SYS_STAT_H 1
+
+/* Define if you have the <sys/types.h> header file.  */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define if you have the <unistd.h> header file.  */
+#define HAVE_UNISTD_H 1
+
+/* Define if you have the <windows.h> header file.  */
+/* #undef HAVE_WINDOWS_H */
+
+/* Define if you have the nsl library (-lnsl).  */
+/* #undef HAVE_LIBNSL */
+
+/* Define if you have the socket library (-lsocket).  */
+/* #undef HAVE_LIBSOCKET */
+
+/* Name of package */
+#define PACKAGE "chasen"
+
+/* Version number of package */
+#define VERSION "2.2.9"
+
--- t/20_SentAlign.t
+++ t/20_SentAlign.t
@@ -20,7 +20,7 @@ my $null = "2> /dev/null >/dev/null";
 system("$UPLUG align/sent -src $DATA/xml/1988de.basic.xml -trg $DATA/xml/1988en.basic.xml -out align.xml $null");
 
 # ignore certainties (may vary on different OSs)
-system("sed 's/certainty=\"[-0-9]*\"//' $DATA/xml/de-en/1988.basic.sent.xml > gold.xml");
+system("sed 's/certainty=\"[-0-9]*\"//' $DATA/xml/de-en/1988.sent.xml > gold.xml");
 system("sed 's/certainty=\"[-0-9]*\"//' align.xml > system.xml");
 
 is( compare( "system.xml", "gold.xml" ),0, "sentence alignment (de-en)" );
@@ -34,7 +34,7 @@ unlink('system.xml');
 
 system("$UPLUG align/hun -src $DATA/xml/1988de.basic.xml -trg $DATA/xml/1988en.basic.xml -out align.xml $null");
 
-is( compare( "align.xml", "$DATA/xml/de-en/1988.basic.hun.xml" ),0, "hunalign (de-en)" );
+is( compare( "align.xml", "$DATA/xml/de-en/1988.hun.xml" ),0, "hunalign (de-en)" );
 
 unlink('align.xml');
 
@@ -43,7 +43,7 @@ unlink('align.xml');
 
 system("$UPLUG align/gma -src $DATA/xml/1988de.basic.xml -trg $DATA/xml/1988en.basic.xml -out align.xml $null");
 
-is( compare( "align.xml", "$DATA/xml/de-en/1988.basic.gma.xml" ),0, "gma (de-en)" );
+is( compare( "align.xml", "$DATA/xml/de-en/1988.gma.xml" ),0, "gma (de-en)" );
 
 unlink('align.xml');
 
